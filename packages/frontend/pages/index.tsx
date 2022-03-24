57ed3ab5673d70abe7c67bd41e24243ca1ba3281
rilexus <stanip@hotmail.de>
2022-03-24 10:46:47 +0100

    feat(frontend): envs

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,10 +1,18 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 import { useEffect } from "react";
+import { USER } from "@ops/types";
+import { API } from "../env";
 
 const Home: NextPage = () => {
   useEffect(() => {
-    console.log("heir");
+    (async () => {
+      fetch(API)
+        .then((res) => res.json())
+        .then((user: USER) => {
+          console.log(user);
+        });
+    })();
   }, []);
 
   return <div className={styles.container}>OPS</div>;
